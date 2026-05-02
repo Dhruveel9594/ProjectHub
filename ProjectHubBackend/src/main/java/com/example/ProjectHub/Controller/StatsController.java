7285cd896c112d1828a387b827de22479ef5d69c
Dhruveel9594 <dhruveeldevaliya@gmail.com>
2026-05-02 12:02:06 +0530

    Stats Controller added

--- ProjectHubBackend/src/main/java/com/example/ProjectHub/Controller/StatsController.java
+++ ProjectHubBackend/src/main/java/com/example/ProjectHub/Controller/StatsController.java
@@ -1,5 +1,23 @@
 package com.example.ProjectHub.Controller;
 
+import com.example.ProjectHub.Service.StatsService;
+import com.example.ProjectHub.dto.StatsDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class StatsController {
 
+    @Autowired
+    private StatsService statsService;
+
+
+    @GetMapping("/stats")
+    public ResponseEntity<StatsDTO> getStats() {
+        return ResponseEntity.ok(statsService.getStats());
+    }
 }
